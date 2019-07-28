c803acfec671499999613c01b1e4e9a28aeb16e6
andres <andres.jorquera@kairosds.com>
2019-07-28 19:58:41 +0200

    bugfix: closes #4

--- src/components/Bot/Bot.js
+++ src/components/Bot/Bot.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ChatBot from 'react-simple-chatbot';
 import ReactGA from 'react-ga';
 import botFlow from './botFlow';
+import styled, { ThemeProvider } from 'styled-components';
 
 let DOMAIN = process.env.DEPLOY_URL || '';
 
@@ -10,13 +11,41 @@ let DOMAIN = process.env.DEPLOY_URL || '';
  */
 DOMAIN = DOMAIN.replace('/undefined', '');
 
+const mainColor = 'rgb(33, 150, 243)';
+
+const theme = {
+	background: '#f5f8fb',
+	fontFamily: 'Helvetica Neue',
+	headerBgColor: mainColor,
+	headerFontColor: '#fff',
+	headerFontSize: '15px',
+	botBubbleColor: mainColor,
+	botFontColor: '#fff',
+	userBubbleColor: '#fff',
+	userFontColor: '#4a4a4a',
+}
+
+const size = 64;
+const CustomDiv = styled.div`
+	.rsc-float-button {
+		width: ${size}px;
+		height: ${size}px;
+	}
+`;
+
 export default class Bot extends React.Component {
 	constructor(props) {
 		super(props);
 		this.ref = React.createRef();
 		this.state = {
-			isOpen: false
+			isOpen: false,
+			reset: false
 		}
+
+		botFlow[5].trigger = () => {
+			this.sendMessage();
+			return 'messageSuccess'
+		};
 	}
 
 	toggleChat(force) {
@@ -47,28 +76,43 @@ export default class Bot extends React.Component {
 	endChat() {
 		setTimeout(() => {
 			this.toggleChat(false);
-		}, 2500);
+			setTimeout(() => {
+				this.reset();
+			}, 500);
+		}, 2000);
+	}
+
+	reset() {
+		this.setState({reset: true});
+		setTimeout(() => {
+			this.setState({reset: false})
+		}, 0);
 	}
 	
 	render () {
-		botFlow[5].trigger = () => {
-			this.endChat();
-			this.sendMessage();
-			return 'messageSuccess'
-		};
+		
+
+		if(this.state.reset) {
+			return (<div></div>);
+		}
+
 		return (
-			<div>
-				<ChatBot 
-					ref={this.ref}
-					floating 
-					opened={this.state.open} 
-					steps={botFlow} 
-					userDelay={700}
-					headerTitle="Scrappy - Personal Assistant"
-					hideUserAvatar={true}
-					handleEnd={this.endChat.bind(this)}
-				/>
-			</div>
+			
+
+			<ThemeProvider theme={theme}>
+				<CustomDiv>
+					<ChatBot 
+						ref={this.ref}
+						floating 
+						opened={this.state.open} 
+						steps={botFlow} 
+						userDelay={700}
+						headerTitle="Scrappy - Personal Assistant"
+						hideUserAvatar={true}
+						handleEnd={this.endChat.bind(this)}
+					/>
+				</CustomDiv>
+			</ThemeProvider>
 		);
 	}
 }
